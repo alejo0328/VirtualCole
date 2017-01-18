b05bcf4aa949f6a9fde97b47c3baf3655e2c0802
kevinPuscan <kpuscano12@unc.edu.pe>
2017-01-17 20:04:28 -0500

    COnfigurando directorio de pruebas

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+/**
+ * Created by PhpStorm.
+ * User: KEVIN
+ * Date: 17/01/2017
+ * Time: 19:54
+ */
+
+
+class FeatureTestCase extends TestCase
+{
+    Use DatabaseMigrations;
+}
