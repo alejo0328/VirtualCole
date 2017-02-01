a78b59799e2154094b4e90617bfcec0f6ddc6431
kevinPuscan <kpuscano12@unc.edu.pe>
2017-01-31 21:36:54 -0500

    mejorando frontend listar colegios

--- resources/views/college/index.blade.php
+++ resources/views/college/index.blade.php
@@ -2,14 +2,35 @@
 @extends('layouts.private.adminCollege.menu')
 
 @section('content')
-    <div class="container">
-        <ul>
-        @foreach($colleges as $college)
-            <li>
-                {{$college->name}}
-            </li>
+    <div class="container col-md-9 center-block quitar-float text-center espacio-arriba">
+
+
+        <table class="table table-hover">
+            <tr>
+                <td>NOMBRE</td>
+                <td>OPERACIONES</td>
+
+            </tr>
+            @foreach($colleges as $college)
+
+                <tr>
+                    <td>{{$college->name}}</td>
+                    <td>
+                        <a href="#" class="btn btn-info" value={{$college->id}}>
+                            <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                        </a>
+                        <a href="#" class="btn btn-danger" value={{$college->id}}>
+                            <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                        </a>
+                    </td>
+
+                </tr>
 
             @endforeach
+        </table>
+
+        <ul>
+
         </ul>
     </div>
 @endsection
